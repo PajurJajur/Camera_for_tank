c9076f9c4301f648c6217117bd51c07f40fb92e4
PajurJajur <133235172+PajurJajur@users.noreply.github.com>
2024-08-05 19:38:54 +0200

    Add files via upload

--- opencvdetekcjakoloru_raspberry_pi.cpp
+++ opencvdetekcjakoloru_raspberry_pi.cpp
@@ -1,145 +1,179 @@
-#include <iostream> 
-#include <stdlib.h>
-#include <opencv2/opencv.hpp> 
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/calib3d.hpp>
-#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include <wiringPi.h>
-#include <wiringSerial.h>
 #include <unistd.h>
-
-
+#include <cmath>
 
 using namespace cv;
 using namespace std;
 
+// Definicje pinów PWM dla serw
+const int SERVO_PIN_X = 12; // GPIO 12 (Pin 32)
+const int SERVO_PIN_Y = 13; // GPIO 13 (Pin 33)
 
+// Zakresy kątów dla serw
+const int SERVO1_MIN_ANGLE = 180; // Min kąt serwa 1
+const int SERVO1_MAX_ANGLE = 0;  // Max kąt serwa 1
 
+const int SERVO2_MIN_ANGLE = 180; // Min kąt serwa 2
+const int SERVO2_MAX_ANGLE = 90;  // Max kąt serwa 2
 
-// Driver code 
-int main()
-{
-    int serial_port;
-    
-    if ((serial_port = serialOpen("/dev/ttyACM0", 9600)) < 0)
-{
-    cerr << "nie da rady kolego\n";
-    return 1;
+// Zakresy PWM dla serw
+const int PWM_MIN = 50;  // Minimalna wartość PWM odpowiadająca 1 ms
+const int PWM_MAX = 250; // Maksymalna wartość PWM odpowiadająca 2 ms
+
+const int CHANGE_THRESHOLD = 50; // Próg zmiany 
+const int SMOOTHING_FACTOR = 8; // Współczynnik wygładzania PWM
+
+int map1(int x, int in_min, int in_max, int out_min, int out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-    
+// Funkcja do konwersji pozycji na kąt w stopniach
+int positionToAngle(int position, int center, int minAngle, int maxAngle) {
+    return map1(position, 0, 2 * center, minAngle, maxAngle);
+}
 
-    int hmin = 0, smin = 73, vmin = 147;
-    int hmax = 179, smax = 255, vmax = 255;
-    int posX_tymczas = 0;
-    int posY_tymczas = 0;
+// Funkcja do konwersji kąta na sygnał PWM
+int angleToPWM(int angle, int minAngle, int maxAngle, int minPWM, int maxPWM) {
+    return map1(angle, minAngle, maxAngle, minPWM, maxPWM);
 
-    Mat image;
-    Mat image1;
-    
-    Mat image2, image3, imagewykryty;
+
+}
+// Funkcja do wygładzania wartości PWM
+int smoothPWM(int currentPWM, int previousPWM, int factor) {
+    return previousPWM + (currentPWM - previousPWM) / factor;
+}
+
+
+float calculateDistance(int x1, int y1, int x2, int y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+// Driver code
+int main() {
+    if (wiringPiSetupGpio() == -1) { // Użycie numeracji pinów GPIO
+        cerr << "Nie można zainicjalizować WiringPi\n";
+        return 1;
+    }
+
+    // Inicjalizacja pinów PWM
+    pinMode(SERVO_PIN_X, PWM_OUTPUT);
+    pinMode(SERVO_PIN_Y, PWM_OUTPUT);
+
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetClock(192);  // Ustawienie preskalera
+    pwmSetRange(1024); // Ustawienie zakresu
+
+    int hmin = 19, smin = 2, vmin = 176;
+    int hmax = 94, smax = 155, vmax = 255;
+
+    Mat image, image1, imagewykryty;
 
     namedWindow("paski_zmian", (640, 200));
     createTrackbar("Hue Min", "paski_zmian", &hmin, 179);
-    createTrackbar("saturation Min", "paski_zmian", &smin, 255);
-    createTrackbar("value Min", "paski_zmian", &vmin, 255);
+    createTrackbar("Saturation Min", "paski_zmian", &smin, 255);
+    createTrackbar("Value Min", "paski_zmian", &vmin, 255);
     createTrackbar("Hue Max", "paski_zmian", &hmax, 179);
-    createTrackbar("saturation Max", "paski_zmian", &smax, 255);
-    createTrackbar("value Max", "paski_zmian", &vmax, 255);
-    
-    
-   
-    
+    createTrackbar("Saturation Max", "paski_zmian", &smax, 255);
+    createTrackbar("Value Max", "paski_zmian", &vmax, 255);
+
     VideoCapture Cam(0);
-    // Show Image inside a window with
-    // the name provided
-    namedWindow("camera",WINDOW_NORMAL);
-    namedWindow("camera1",WINDOW_NORMAL);
-    resizeWindow("camera",800,600);
-    resizeWindow("camera1",800,600);
+    if (!Cam.isOpened()) {
+        cerr << "Nie można otworzyć kamery\n";
+        return 1;
+    }
+
+    namedWindow("camera", WINDOW_NORMAL);
+    namedWindow("camera1", WINDOW_NORMAL);
+    resizeWindow("camera", 800, 600);
+    resizeWindow("camera1", 800, 600);
     
+    int prevAngleX = (SERVO1_MIN_ANGLE + SERVO1_MAX_ANGLE) / 2;
+    int prevAngleY = (SERVO2_MIN_ANGLE + SERVO2_MAX_ANGLE) / 2;
+    int prevPWM_X = angleToPWM(prevAngleX, SERVO1_MIN_ANGLE, SERVO1_MAX_ANGLE, 50, 250);
+    int prevPWM_Y = angleToPWM(prevAngleY, SERVO2_MIN_ANGLE, SERVO2_MAX_ANGLE, 250, 150);
+    
+    pwmWrite(SERVO_PIN_X, 150);
+    pwmWrite(SERVO_PIN_Y, 175);
     
     while (true) {
-
-
-        
         Cam.read(image);
-        waitKey(30); // 30 ms opóźnienia
-        cvtColor(image, image1,COLOR_BGR2HSV);
+        if (image.empty()) {
+            cerr << "Nie można odczytać obrazu z kamery\n";
+            break;
+        }
+
+        cvtColor(image, image1, COLOR_BGR2HSV);
         Scalar dolna(hmin, smin, vmin);
         Scalar gorna(hmax, smax, vmax);
 
         inRange(image1, dolna, gorna, imagewykryty);
+
+        // Filtracja szumów
         erode(imagewykryty, imagewykryty, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
         dilate(imagewykryty, imagewykryty, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-        //morphological closing (removes small holes from the foreground)
         dilate(imagewykryty, imagewykryty, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
         erode(imagewykryty, imagewykryty, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-        
-
-
 
         Moments oMoments = moments(imagewykryty);
-
         double dM01 = oMoments.m01;
         double dM10 = oMoments.m10;
         double dArea = oMoments.m00;
 
-        if (dArea > 20000)
-        {
-            //calculate the position of the ball
+        if (dArea > 10000) {
             int posX = dM10 / dArea;
             int posY = dM01 / dArea;
 
+            int centerX = image.cols / 2;
+            int centerY = image.rows / 2;
 
+            float distance = calculateDistance(posX, posY, centerX, centerY);
+            
+            int angleX = positionToAngle(posX, centerX, SERVO1_MIN_ANGLE, SERVO1_MAX_ANGLE);
+            int angleY = positionToAngle(posY, centerY, SERVO2_MIN_ANGLE, SERVO2_MAX_ANGLE);
+
+
+            int currentPWM_X = angleToPWM(angleX, SERVO1_MIN_ANGLE, SERVO1_MAX_ANGLE, 250, 50);
+            int currentPWM_Y = angleToPWM(angleY, SERVO2_MIN_ANGLE, SERVO2_MAX_ANGLE, 150, 210);
+            
+            // moze do wyrzucenia
+            //int pwmX = angleToPWM(angleX, SERVO1_MIN_ANGLE, SERVO1_MAX_ANGLE, 100, 200);
+            //int pwmY = angleToPWM(angleY, SERVO2_MIN_ANGLE, SERVO2_MAX_ANGLE, 250, 150);
+            
+            if(currentPWM_Y >210) currentPWM_Y =210;
+            
+            cout << "posX: " << posX << " posY: " << posY << endl;
+            cout << "angleX: " << angleX << " angleY: " << angleY << endl;
+            cout << "pwmX: " << currentPWM_X << " pwmY: " << currentPWM_Y << endl;
+            cout << "CenterX: " << centerX << " CenterY: " << centerY << endl;
+             cout << "distance: " << distance<<endl;
+             
+            if (distance >= CHANGE_THRESHOLD) {
+                int smoothPWM_X = smoothPWM(currentPWM_X, prevPWM_X, SMOOTHING_FACTOR);
+                int smoothPWM_Y = smoothPWM(currentPWM_Y, prevPWM_Y, SMOOTHING_FACTOR);
+                cout << "smoothPWM_X: " << smoothPWM_X << " smoothPWM_Y " << smoothPWM_Y << endl;
+                pwmWrite(SERVO_PIN_X, smoothPWM_X);
+                pwmWrite(SERVO_PIN_Y, smoothPWM_Y);
+
+                // Aktualizacja poprzednich kątów i PWM
+                prevAngleX = angleX;
+                prevAngleY = angleY;
+                prevPWM_X = smoothPWM_X;
+                prevPWM_Y = smoothPWM_Y;
+
+                // Dodanie opóźnienia po zmianie wartości PWM
+                usleep(20000); // 20 ms
+                }
+            
+            
+            
             ellipse(image, Point(posX, posY), Size(100, 100), 0, 0, 360, Scalar(255, 0, 0), 6);
-            line(image, Point(posX-100, posY), Point(posX+100,posY), Scalar(255,0,0),3);
-            line(image, Point(posX, posY-100), Point(posX,posY+100), Scalar(255,0,0),3);
-
-            if (posX - posX_tymczas > 2 || posY - posY_tymczas > 2)        
-            {
-                cout << "obracaj w lewo" << endl;
-                
-                string pozycja = to_string(posX)+","+to_string(posY)+"\n";
-                
-                serialPuts(serial_port, pozycja.c_str());
-                
-                std::cout << "Wrote: " << pozycja.c_str() ;
-                
-                
-            
-            }
-            if (posX_tymczas - posX > 2|| posY_tymczas - posY > 2) 
-            {
-                cout << "obracaj w prawo" << endl;
-                
-                string pozycja2 = to_string(posX) + "," + to_string(posY) + "\n";
-                
-                serialPuts(serial_port, pozycja2.c_str());
-                
-                std::cout << "Wrote: " << pozycja2.c_str() ;
-                
-            }
-            posX_tymczas = posX;
-            posY_tymczas = posY;
-
-
-          
-            
-
+            line(image, Point(posX - 100, posY), Point(posX + 100, posY), Scalar(255, 0, 0), 3);
+            line(image, Point(posX, posY - 100), Point(posX, posY + 100), Scalar(255, 0, 0), 3);
         }
-        //Canny(image1,image3,20,80);
+
         imshow("camera", image);
         imshow("camera1", imagewykryty);
         if (waitKey(1) == 27) break;
-    }
-    
-    waitKey(0);
-    // Wait for any keystroke
-    serialClose(serial_port);
-    return 0;
-    
-
-}
+}}
